ec11a548cd4fee58aedb657416de0cf4d2226707
Fernando Matos <fernando.matos221@gmail.com>
2026-09-10 22:20:42 -0300

    Adiciona telas de replicação de processo (view e view em tabela)

---
+++ src/view/TelaReplicacaoProcessoView.java
@@ -0,0 +1,76 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TelaReplicacaoProcessoView extends JFrame {
+
+    private JTextField txfId;
+    private JTextField txfProcesso;
+    private JTextField txfDescricao;
+    private JCheckBox chkHabilitado;
+
+    private JButton btnSalvar;
+    private JButton btnAdicionar;
+    private JButton btnBuscar;
+    private JButton btnExcluir;
+
+   public TelaReplicacaoProcessoView() {
+       setTitle("Cadastro de Processos");
+       setSize(620, 320);
+       setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+       setLocationRelativeTo(null);
+       setResizable(false);
+       setLayout(null);
+
+       btnBuscar = new JButton("BUSCAR");
+       btnAdicionar = new JButton("ADICIONAR");
+       btnSalvar = new JButton("SALVAR");
+       btnExcluir = new JButton("EXCLUIR");
+
+       btnBuscar.setBounds(10, 10, 130, 30);
+       btnAdicionar.setBounds(150, 10, 130, 30);
+       btnSalvar.setBounds(290, 10, 130, 30);
+       btnExcluir.setBounds(430, 10, 130, 30);
+
+       getContentPane().add(btnBuscar);
+       getContentPane().add(btnAdicionar);
+       getContentPane().add(btnSalvar);
+       getContentPane().add(btnExcluir);
+
+       JLabel lblId = new JLabel("ID:");
+       lblId.setBounds(10, 70, 120, 25);
+       getContentPane().add(lblId);
+
+       txfId = new JTextField();
+       txfId.setBounds(140, 70, 200, 25);
+       getContentPane().add(txfId);
+
+       JLabel lblProcesso = new JLabel("PROCESSO: ");
+       lblProcesso.setBounds(10, 105, 120, 25);
+       getContentPane().add(lblProcesso);
+
+       txfProcesso = new JTextField();
+       txfProcesso.setBounds(140, 105, 420, 25);
+       getContentPane().add(txfProcesso);
+
+       JLabel lblDescricao = new JLabel("DESCRIÇÃO: ");
+       lblDescricao.setBounds(10, 140, 120, 25);
+       getContentPane().add(lblDescricao);
+
+       txfDescricao = new JTextField();
+       txfDescricao.setBounds(140, 140, 420, 25);
+       getContentPane().add(txfDescricao);
+
+       chkHabilitado = new JCheckBox("HABILITADO");
+       chkHabilitado.setBounds(10,175,120,25);
+       getContentPane().add(chkHabilitado);
+
+   }
+
+   public static void main(String[] args){
+       SwingUtilities.invokeLater(() -> new TelaReplicacaoProcessoView().setVisible(true));
+   }
+
+
+}
